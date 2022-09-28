7c38c7d9f0e6c4b1f414ce82678ff1ee199bd348
yasmin <yasmin.oliver210@gmail.com>
2022-09-28 17:24:02 -0300

    prova de pw

--- calculadora/script.js
+++ calculadora/script.js
@@ -1,3 +1,10 @@
+class calculadora{
+    somar(n1,n2){return n1+n2}
+    subtrair(n1,n2){return n1-n2}
+    dividir(n1,n2){return n1/n2}
+    multiplicar(n1,n2){return n1*n2}
+}
+
 function displaynum(n1) {
     calcal.text1.value = calcal.text1.value + n1;
 }
