6570813dfdfbb6e4c4a8f9021682fc59d62c328a
yasmin <yasmin.oliver210@gmail.com>
2022-09-28 17:30:41 -0300

    prova de pw1

--- calculadora/script.js
+++ calculadora/script.js
@@ -1,8 +1,18 @@
-class calculadora{
-    somar(n1,n2){return n1+n2}
-    subtrair(n1,n2){return n1-n2}
-    dividir(n1,n2){return n1/n2}
-    multiplicar(n1,n2){return n1*n2}
+class Calculadora{
+
+    somar(n1, n2){
+        return n1+n2
+    }
+
+    subtrair(n1, n2){
+        return n1-n2
+    }
+    multiplicar(n1, n2){
+        return n1*n2
+    }
+    dividir (n1, n2){
+        return n1/n2
+    }
 }
 
 function displaynum(n1) {
